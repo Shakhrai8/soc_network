5977cc878359f506e2cefa81e84b6ce517276d9b
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-25 19:55:54 +0100

    add new methods for posts database, add new tests for posts database, passes tests

--- lib/post_repository.rb
+++ lib/post_repository.rb
@@ -1,50 +1,71 @@
+require_relative './post'
+
 class PostRepository
 
   def all
     posts = []
-    sql = 'SELECT id, title, contents, views, accout_id FROM posts;'
+    sql = 'SELECT id, title, contents, views, account_id FROM posts;'
     result_set = DatabaseConnection.exec_params(sql, [])
   
     result_set.each do |inst|
       post = Post.new
   
-      account.id = inst['id']
-      account.title = inst['title']
-      account.contents = inst['contents']
+      post.id = inst['id']
+      post.title = inst['title']
+      post.contents = inst['contents']
+      post.views = inst['views']
+      post.account_id = inst['account_id']
   
-      accounts << account
+      posts << post
     end
   
-    return accounts 
+    return posts 
   end
 
-
   def find(id)
-    # id is an integer representing the number of id to search for
-    # SELECT title, contents, views, accout_id FROM posts WHERE id = $1;'
-    # returns an instance of Post object
+    posts = []
+    sql = 'SELECT title, contents, views, account_id FROM posts WHERE id = $1;'
+    result_set = DatabaseConnection.exec_params(sql, [id])
+
+    result_set.each do |inst|
+      post = Post.new
+
+      post.title = inst['title']
+      post.contents = inst['contents']
+      post.views = inst['views']
+      post.account_id = inst['account_id']
+
+      posts << post
+    end
+
+    return posts 
   end
 
   def create(post)
-    # Executes the SQL query;
-    # INSERT INTO posts (title, contents, views, accout_id) VALUES ($1, $2, $3, $4);
+    
+    sql = 'INSERT INTO posts (title, contents, views, account_id) VALUES ($1, $2, $3, $4);'
+    params = [post.title, post.contents, post.views, post.account_id]
 
-    # Doesn't nned to return anything 
+    DatabaseConnection.exec_params(sql, params)
+
+    return nil
+
   end
 
   def delete(id)
-    # Executes the SQl;
-    # DELETE FROM posts WHERE id = $1;
+    sql = 'DELETE FROM posts WHERE id = $1;'
 
-    # Returns nothing (only deletes the record)
+    DatabaseConnection.exec_params(sql, [id])
+
+    return nil
   end
-
+# UPDATE posts SET title = $1, contents= $2, views = $3, account_id =$4 WHERE id = $5;
   def update(post)
-    # Executes the SQL;
-    # UPDATE posts SET title = $1, contents= $2, views = $3, account_id =$4 WHERE id = $5;
-
-    # Returns nothing(only updates)
+    sql = 'UPDATE posts SET title = $1, contents= $2, views = $3, account_id =$4 WHERE id = $5;'
+    params = [post.title, post.contents, post.views, post.account_id, post.id]
+  
+    DatabaseConnection.exec_params(sql, params)
+  
+    return nil
   end
-
-
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -21,5 +21,73 @@ RSpec.describe PostRepository do
     expect(post.first.title).to eq('title_1') 
     expect(post.first.contents).to eq('con_1') 
     expect(post.first.views).to eq('123')
+    expect(post.first.account_id).to eq('1')
+  end
+
+  it "returns an instance by searching id" do
+    repo = PostRepository.new
+
+    post = repo.find(2)
+    expect(post.length).to eq 1 # => 2
+    expect(post.first.title).to eq('title_2') 
+    expect(post.first.contents).to eq('con_2') 
+    expect(post.first.views).to eq('234')
+    expect(post.first.account_id).to eq('2')
+  end
+
+  it "creates a new instance in the database and returns it" do
+    repo = PostRepository.new
+
+    post = Post.new
+    post.title = 'super_new_title'
+    post.contents = 'also_something_cool'
+    post.views = '9999'
+    post.account_id = '2'
+    
+    repo.create(post)
+    
+    post_new = repo.all
+    expect(post_new.last.title).to eq('super_new_title') # => 'email56@gmail.com'
+    expect(post_new.last.contents).to eq('also_something_cool')
+    expect(post_new.last.views).to eq('9999')
+    expect(post_new.last.account_id).to eq('2')
+
+  end
+
+  it "deletes an instance by its id" do
+    repo = PostRepository.new
+
+    id_to_delete = 1
+    
+    repo.delete(id_to_delete)
+    
+    all_posts = repo.all
+    expect(all_posts.length).to eq 1 # => 1
+    expect(all_posts.first.id).to eq('2') # => '2'
+    expect(all_posts.first.title).to eq('title_2')
+    expect(all_posts.first.contents).to eq('con_2') 
+    expect(all_posts.first.views).to eq('234')
+    expect(all_posts.first.account_id).to eq('2')
+  end
+
+  it "updates an instance by its id" do
+    repo = PostRepository.new
+
+    post = repo.find(1)
+
+    post[0].title = 'not_title'
+    post[0].contents = 'blabla'
+    post[0].views = '88888'
+    post[0].account_id = '1'
+    post[0].id = 1
+
+
+    repo.update(post[0])
+
+    updated_post = repo.find(1)
+    expect(updated_post[0].title).to eq 'not_title'
+    expect(updated_post[0].contents).to eq 'blabla'
+    expect(updated_post[0].views).to eq '88888'
+    expect(updated_post[0].account_id).to eq '1'
   end
 end
